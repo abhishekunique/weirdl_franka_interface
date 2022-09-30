e662a78140beed63bcb04ff2b023f8a85bd4909a
ahmeda14960 <ahmedah@stanford.edu>
2022-09-29 19:42:31 -0700

    simple test for cluster

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from robot_env import RobotEnv
-from controllers.xbox_controller import XboxController
+#from controllers.xbox_controller import XboxController
 import numpy as np
 import time
 
-controller = XboxController(DoF=3)
+#controller = XboxController(DoF=3)
 print(f'init env')
 ip_add = '172.16.0.2'
 env = RobotEnv()
@@ -16,5 +16,5 @@ env.reset()
 max_steps = 10000
 for i in range(max_steps):
 	obs = env.get_state()
-	action = controller.get_action()
+	action = np.random.uniform(0, 0.5, (4, ))
 	env.step(action)
